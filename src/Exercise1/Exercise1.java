47e269e4dc81e7803d2e378dda392466dee6df22
gonzalo.prada <gonzalo.prada@106-22.info2>
2025-09-19 12:50:15 +0200

    Exercise1 Editado 19/9/25

--- src/Exercise1/Exercise1.java
+++ src/Exercise1/Exercise1.java
@@ -3,7 +3,7 @@ package Exercise1;
 public class Exercise1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello!,How are you?");
+		System.out.print("Hello!,How are you?");
 		System.out.println("Fine Thanks.");
 		
 	}
