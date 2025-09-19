0eb0407e9f5d43641f43e9695d90668bf3ac7161
gonzalo.prada <gonzalo.prada@106-22.info2>
2025-09-19 12:44:57 +0200

    Exercise1 19/9/2025

---
+++ src/Exercise1/Exercise1.java
@@ -0,0 +1,11 @@
+package Exercise1;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!,How are you?");
+		System.out.println("Fine Thanks.");
+		
+	}
+
+}
